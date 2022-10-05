66bbb77ae8d6898229cc8c09897205f894edcd89
DESKTOP-88A6AV6\kerberos <hyeongseokgim@gmail.com>
2022-10-05 17:47:33 +0900

    퇴근 전

--- projects/mysite/fanMonitoring/views.py
+++ projects/mysite/fanMonitoring/views.py
@@ -25,6 +25,23 @@ def registrationServer(request):
     pass
 
 def cmh(request):
+    serverType = request.GET.get('serverType', '')
+    sdps = Cmh.objects.filter(serverId__serverTypeId = 1).last() #최근값만 가져옴.
+    fdps = Cmh.objects.filter(serverId__serverTypeId = 2).last()
+    drfs = Cmh.objects.filter(serverId__serverTypeId = 3).values().last()
+    sdrs = Cmh.objects.filter(serverId__serverTypeId = 4).values().last()
+    efss = Cmh.objects.filter(serverId__serverTypeId = 5).values().last()
+    rcoms = Cmh.objects.filter(serverId__serverTypeId = 6).values().last()
+    byps = Cmh.objects.filter(serverId__serverTypeId = 7).values().last()
+    tsvs = Cmh.objects.filter(serverId__serverTypeId = 8).values().last()
+    apps = Cmh.objects.filter(serverId__serverTypeId = 9).values().last()
+    nmss = Cmh.objects.filter(serverId__serverTypeId = 10).values().last()
+    pmss = Cmh.objects.filter(serverId__serverTypeId = 11).values().last()
+    wass = Cmh.objects.filter(serverId__serverTypeId = 12).values().last()
+    etcs = Cmh.objects.filter(serverId__serverTypeId = 13).values().last()
+    context = {'sdps' : sdps, 'fdps' : fdps, 'drfs' : drfs, 'sdrs' : sdrs, 'efss' : efss, 'rcoms' : rcoms, 'byps' : byps,\
+               'tsvs' : tsvs, 'apps' : apps, 'nmss' : nmss, 'pmss' : pmss, 'wass' : wass, 'etcs' : etcs}
+    return render(request, 'fanMonitoring/cmh.html', context)
     pass
 
 def network(request):
